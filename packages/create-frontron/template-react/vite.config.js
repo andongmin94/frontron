35b91442905712b2448d9cd3433d7e37341dcc89
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-05-06 01:55:41 +0900

    build: 패키지 설정 정리 packages package.json

--- packages/create-frontron/template-react/vite.config.js
+++ packages/create-frontron/template-react/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })

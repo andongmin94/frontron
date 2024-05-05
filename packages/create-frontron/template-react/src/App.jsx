96f4dcd7f15a56e6c8a33a7dba7a4bb549b73e1b
김상훈 <k1016h@naver.com>
2024-05-06 01:55:41 +0900

    패키지 설정 정리 packages package.json

--- packages/create-frontron/template-react/src/App.jsx
+++ packages/create-frontron/template-react/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import frontronLogon from "/icon.png";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="app">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={frontronLogon} className="logo frontron" alt="Frontron logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={viteLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Frontron + React + Vite</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;

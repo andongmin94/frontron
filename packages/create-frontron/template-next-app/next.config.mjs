28c5973158d2b1fb22e44782df3852015928ecfe
andongmin94 <andongmin94@gmail.com>
2024-05-08 00:02:59 +0900

    Update cf ver (next app and page router js template)

---
+++ packages/create-frontron/template-next-app/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a railing slash to all paths '/about' -> '/about/'
+    // trailingSlash: true,
+    // Optional: Change the output directory 'out' -> 'dist'
+    // distDir: 'dist',
+    images: {
+      unoptimized: true
+    }
+  }
+  
+  export default nextConfig;
+  

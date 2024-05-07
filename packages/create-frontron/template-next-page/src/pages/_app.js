c92d63a3b9f7c48eca96132a6cc3cb73021d3540
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-05-08 00:02:59 +0900

    docs: 문서 정리 README

---
+++ packages/create-frontron/template-next-page/src/pages/_app.js
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+import Head from "next/head";
+import TitleBar from "@/components/TitleBar";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Frontron</title>
+        <meta property="og:title" content="Frontron" key="title" />
+        <link rel="icon" type="image" href="/icon.png" />
+        <link rel="shortcut icon" href="/icon.png" />
+      </Head>
+      <TitleBar />
+      <Component {...pageProps} />
+    </>
+  );
+}

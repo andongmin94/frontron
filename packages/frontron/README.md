0e6636c8fa8f0516e88e676ae327fd907fa011a9
andongmin94 <andongmin94@gmail.com>
2024-05-11 00:38:17 +0900

    Rollback

---
+++ packages/frontron/README.md
@@ -0,0 +1,1 @@
+# frontron <a href="https://npmjs.com/package/frontron"><img src="https://img.shields.io/npm/v/frontron" alt="npm package"></a>
--- packages/frontron/src/main.tsx
+++ packages/frontron/src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import "@/globals.css";
 //////////////// electron components ////////////////
 import TitleBar from "@/components/TitleBar";
 /////////////////////////////////////////////////////
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <TitleBar />
     <App />

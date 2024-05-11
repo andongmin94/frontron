27589242cdb6c31fe7f34a371e552001656eb0c6
andongmin94 <andongmin94@gmail.com>
2024-05-11 23:55:30 +0900

    문서 정리 README

---
+++ docs/_data/team.js
@@ -0,0 +1,56 @@
+export const core = [
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/110483588?v=4',
+    name: '김상훈',
+    title: 'Developer',
+    desc: 'The knight of Information processing.',
+    links: [
+      { icon: 'github', link: 'https://github.com/andongmin94' },
+    ],
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/128280944?v=4',
+    name: '김경범',
+    title: 'Developer',
+    desc: '안녕하세요! 폭발적인 성장을 원하는 개발자 김경범입니다.',
+    links: [
+      { icon: 'github', link: 'https://github.com/dreamingbeom' },
+    ],
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/128338647?v=4',
+    name: '박정환',
+    title: 'Developer',
+    desc: '안녕하세요, 저는 이번에 python으로 개발에 입문한 박정환 입니다.',
+    links: [
+      { icon: 'github', link: 'https://github.com/Nam4o' },
+    ],
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/139419039?v=4',
+    name: '임승환',
+    title: 'Developer',
+    desc: '안녕하세요! 학습에 적극적인 개발자 임승환입니다!',
+    links: [
+      { icon: 'github', link: 'https://github.com/Lim-seunghwan99' },
+    ],
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/119795734?v=4',
+    name: '조수현',
+    title: 'Developer',
+    desc: '내가 하늘에 서겠다.',
+    links: [
+      { icon: 'github', link: 'https://github.com/chosuhyeon0812' },
+    ],
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/139519997?v=4',
+    name: '편수지',
+    title: 'Developer',
+    desc: '내가 하늘에 서겠다.',
+    links: [
+      { icon: 'github', link: 'https://github.com/pyunsusie' },
+    ],
+  },
+]

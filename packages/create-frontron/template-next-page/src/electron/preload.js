150e72952d04fb2279e90a5a84de74a4df2f5383
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-05-16 12:29:25 +0900

    docs: 문서 정리 README

---
+++ packages/create-frontron/template-next-page/src/electron/preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('frontron', {
+  send: (channel, data) => {
+    ipcRenderer.send(channel, data);
+  },
+  on: (channel, func) => {
+    ipcRenderer.on(channel, (event, ...args) => func(...args));
+  }
+});

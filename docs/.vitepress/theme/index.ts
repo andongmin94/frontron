95f40fabcf4edea51209b4720d5bf46d87242d72
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-06-27 21:32:37 +0900

    docs: 문서 정리 docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme-without-fonts'
+import DefaultTheme from 'vitepress/theme'
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import '@shikijs/vitepress-twoslash/style.css'
 import './styles/vars.css'

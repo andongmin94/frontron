be44a640b352d8e996504fd1bc5b76f10c846984
안동민 <andongmin94@gmail.com>
2024-08-11 03:40:00 +0900

    패키지 설정 정리 packages package.json

--- packages/create-frontron/template-react-ts/src/main.tsx
+++ packages/create-frontron/template-react-ts/src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import "@/globals.css";
+import "@/globals.css"
 //////////////// electron components ////////////////
-import TitleBar from "@/components/TitleBar";
+import TitleBar from "@/components/TitleBar"
 /////////////////////////////////////////////////////
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

acd84b8b3f7b0d7bf0170c67e15f7c503c7a6051
안동민 <andongmin94@gmail.com>
2024-08-13 17:25:58 +0900

    문서 정리 README

--- packages/create-frontron/template-react-ts/vite.config.ts
+++ packages/create-frontron/template-react-ts/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/",
+  base: "./",
   server: {
     port: 3000,
     host: "0.0.0.0",

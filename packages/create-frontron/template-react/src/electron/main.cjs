87ae24346220632b8656ecfc3bbd58049c096c90
안동민 <andongmin94@gmail.com>
2024-08-18 15:46:19 +0900

    to 0.2.8

--- packages/create-frontron/template-react/src/electron/main.cjs
+++ packages/create-frontron/template-react/src/electron/main.cjs
@@ -1,6 +1,12 @@
 // 일렉트론 모듈
-const path = require("node:path");
+const path = require("path");
 const { app, BrowserWindow, ipcMain, Tray, Menu, nativeImage } = require("electron");
+const fs = require('fs');
+
+// package.json 파일 읽기
+const templateDir = __dirname; // templateDir 경로 설정
+const packageJsonPath = path.join(templateDir, '../../package.json');
+const pkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
 // 환경 변수 설정
 require("dotenv").config();
@@ -70,7 +76,7 @@ app.whenReady().then(createWindow).then(() => {
 
   // 트레이 세팅
   const tray = new Tray(nativeImage.createFromPath(path.join(__dirname, "../../public/icon.png")));
-  tray.setToolTip("Frontron");
+  tray.setToolTip(pkg.build.productName);
   tray.on("double-click", () => mainWindow.show());
   tray.setContextMenu(
     Menu.buildFromTemplate([

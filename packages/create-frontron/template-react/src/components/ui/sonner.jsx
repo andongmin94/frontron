059b1dce74739556acaf45d6b4b0b680281d11c3
안동민 <andongmin94@gmail.com>
2024-11-27 17:01:43 +0900

    패키지 설정 정리 packages package.json

--- packages/create-frontron/template-react/src/components/ui/sonner.jsx
+++ packages/create-frontron/template-react/src/components/ui/sonner.jsx
@@ -1,11 +1,14 @@
-import { useTheme } from "next-themes";
-import { Toaster as Sonner } from "sonner";
+"use client";
+import { useTheme } from "next-themes"
+import { Toaster as Sonner } from "sonner"
 
-const Toaster = ({ ...props }) => {
-  const { theme = "system" } = useTheme();
+const Toaster = ({
+  ...props
+}) => {
+  const { theme = "system" } = useTheme()
 
   return (
-    <Sonner
+    (<Sonner
       theme={theme}
       className="toaster group"
       toastOptions={{
@@ -19,9 +22,8 @@ const Toaster = ({ ...props }) => {
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
       }}
-      {...props}
-    />
+      {...props} />)
   );
-};
+}
 
-export { Toaster };
+export { Toaster }

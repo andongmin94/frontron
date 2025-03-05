c3207fcaae0879cdca1b0f6c6162f5da5e3dd621
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-03-06 00:48:57 +0900

    build: 패키지 설정 정리 package.json

---
+++ packages/create-frontron/template/src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App.tsx";
+
+import "@/globals.css";
+
+//////////////// electron components ////////////////
+import TitleBar from "@/components/TitleBar";
+
+/////////////////////////////////////////////////////
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <TitleBar />
+    <App />
+  </>,
+);

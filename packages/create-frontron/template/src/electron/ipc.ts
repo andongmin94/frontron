721400df015f5dbac7c7bcbd8bead605846d2782
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-05-14 12:03:57 +0900

    build: 릴리즈 전 정리 packages package.json

--- packages/create-frontron/template/src/electron/ipc.ts
+++ packages/create-frontron/template/src/electron/ipc.ts
@@ -1,10 +1,13 @@
 import { ipcMain } from 'electron';
 
 /**
- * IPC 이벤트 핸들러 설정
+ * 개발 환경용 메뉴 설정
  * @param {Function} getMainWindow - 메인 윈도우 객체를 반환하는 함수
  */
-export function setupIpcHandlers(getMainWindow) {
+interface MainWindowGetter {
+  (): Electron.BrowserWindow | null; // 메인 윈도우 객체를 반환하는 함수
+}
+export function setupIpcHandlers(getMainWindow: MainWindowGetter) {
   ipcMain.on('hidden', () => {
     getMainWindow()?.hide();
   });

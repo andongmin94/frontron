5abff1386a9d108a17f5593070ec9f1a01ac0442
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-06-30 16:50:24 +0900

    refactor: 템플릿 정리 template

--- packages/create-frontron/template/src/main.tsx
+++ packages/create-frontron/template/src/main.tsx
@@ -6,11 +6,12 @@ import "@/globals.css";
 
 //////////////// electron components ////////////////
 import TitleBar from "@/components/TitleBar";
+const { ipcRenderer } = require("electron");
 /////////////////////////////////////////////////////
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <TitleBar />
-    <App />
+    <TitleBar ipcRenderer={ipcRenderer} />
+    <App ipcRenderer={ipcRenderer} />
   </>
 );

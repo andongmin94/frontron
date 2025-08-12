c777046c47bbc44d7a922188dc7d75bb106f0275
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-08-12 21:27:05 +0900

    refactor: 템플릿 정리 template

--- packages/create-frontron/template/src/electron/preload.ts
+++ packages/create-frontron/template/src/electron/preload.ts
@@ -1,8 +1,10 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('electron', {
+contextBridge.exposeInMainWorld("electron", {
   send: (channel: any, data: any) => ipcRenderer.send(channel, data),
-  on: (channel: any, func: (...args: any[]) => any) => ipcRenderer.on(channel, (event: any, ...args: any[]) => func(...args)),
-  get: (key: any) => ipcRenderer.invoke('get-value', key),
-  removeListener: (channel: any, func: any) => ipcRenderer.removeListener(channel, func),
+  on: (channel: any, func: (...args: any[]) => any) =>
+    ipcRenderer.on(channel, (event: any, ...args: any[]) => func(...args)),
+  get: (key: any) => ipcRenderer.invoke("get-value", key),
+  removeListener: (channel: any, func: any) =>
+    ipcRenderer.removeListener(channel, func),
 });

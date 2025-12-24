dfeb1529946d4c8e7f70288ef1913cc9ec0e2455
안동민 <andongmin94@gmail.com>
2025-12-24 14:12:17 +0900

    f

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,14 +4,14 @@ import { buildEnd } from "./buildEnd.config";
 const ogTitle = "Frontron";
 const ogDescription = "Desktop App Template";
 const ogUrl = "https://frontron.andongmin.com";
-const ogImage = "https://frontron.andongmin.com/logo.svg";
+const ogImage = "https://frontron.andongmin.com/logo.png";
 
 export default defineConfig({
   title: "Frontron",
   description: "Desktop App Template",
 
   head: [
-    ["link", { rel: "icon", type: "image/svg+xml", href: "/logo.svg" }],
+    ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
     [
       "link",
       { rel: "alternate", type: "application/rss+xml", href: "/blog.rss" },

f53eb87a73b699a526309baa459e2caa49e7f286
안동민 <andongmin94@gmail.com>
2026-07-13 16:33:07 +0900

    refactor(create-frontron): tighten scaffold and package surface

--- create-frontron/build.config.ts
+++ create-frontron/build.config.ts
@@ -4,14 +4,9 @@ export default defineBuildConfig({
   entries: ['src/index'],
   clean: true,
   rollup: {
-    inlineDependencies: true,
     esbuild: {
       target: 'node22',
       minify: true,
     },
-  },
-  alias: {
-    // We can use the non-transpiled entry because Node 22+ is required.
-    prompts: 'prompts/lib/index.js',
   },
 })
